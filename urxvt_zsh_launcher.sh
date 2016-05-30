71b7520962ea88099020ce56aedfa48791a77e4a
Conor Finn <finnconor@gmail.com>
2016-05-30 23:13:07 +1000

    Fixed typo in prior commit

--- urxvt_zsh_launcher.sh
+++ urxvt_zsh_launcher.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-urxvt -T "urxvt_zsh_launcher" -geometry 239x10+0+20 -e sh -c "ZDOTDIR=$DIRcd zsh -i -t" 
+urxvt -T "urxvt_zsh_launcher" -geometry 239x10+0+20 -e sh -c "ZDOTDIR=$DIR zsh -i -t" 
